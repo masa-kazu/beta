05789cbbfe27a054f7117899eabd0202fa24ea1d
masa-kazu <tashikazu1990@gmail.com>
2022-07-07 21:42:05 +0900

    Nabvar作成

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,61 @@
 import React from "react";
-import Task from "./Task";
+import { Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import "./App.css";
 
-const App = () => {
+const Nabvar = styled.nav`
+  background: #026aa7;
+  min-height: 8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`;
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`;
+
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
+`;
+
+function App() {
   return (
     <>
-      <h1>タスク一覧</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
+      <Nabvar>
+        <Logo>Food</Logo>
+        <NavItems>
+          <NavItem>
+            <Link to="/foods">Foods</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/foods/new">Add New Food</Link>
+          </NavItem>
+        </NavItems>
+      </Nabvar>
     </>
   );
-};
+}
 
 export default App;
