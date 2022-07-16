dc1b1351c397d33afa5a855a6f77a91f4b8f383b
masa-kazu <tashikazu1990@gmail.com>
2022-07-16 23:39:50 +0900

    EditFood修正

--- app/javascript/components/EditFood.js
+++ app/javascript/components/EditFood.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
-//import { toast } from "react-toastify";
-//import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const InputName = styled.input`
   font-size: 20px;
@@ -18,7 +18,7 @@ const CurrentStatus = styled.div`
   font-weight: bold;
 `;
 
-const IsCompeletedButton = styled.button`
+const IsCompletedButton = styled.button`
   color: #fff;
   font-weight: 500;
   font-size: 17px;
@@ -51,8 +51,115 @@ const DeleteButton = styled.button`
   cursor: pointer;
 `;
 
-function EditFood() {
-  return <div>EditFood</div>;
+toast.configure();
+
+function EditFood(props) {
+  const initialFoodState = {
+    id: null,
+    content: "",
+    complete: false,
+  };
+
+  const [currentFood, setCurrentFood] = useState(initialFoodState);
+
+  const notify = () => {
+    toast.success("Food successfully updated!", {
+      position: "bottom-center",
+      hideProgressBar: true,
+    });
+  };
+
+  const getFood = (id) => {
+    axios
+      .get(`/api/v1/foods/${id}`)
+      .then((resp) => {
+        setCurrentFood(resp.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getFood(props.match.params.id);
+  }, [props.match.params.id]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setCurrentFood({ ...currentFood, [name]: value });
+  };
+
+  const updateIsCompleted = (val) => {
+    var data = {
+      id: val.id,
+      content: val.content,
+      complete: !val.complete,
+    };
+    axios.patch(`/api/v1/foods/${val.id}`, data).then((resp) => {
+      setCurrentFood(resp.data);
+    });
+  };
+
+  const updateFood = () => {
+    axios
+      .patch(`/api/v1/foods/${currentFood.id}`, currentFood)
+      .then((resp) => {
+        notify();
+        props.history.push("/foods");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const deleteFood = () => {
+    const sure = window.confirm("Are you sure?");
+    if (sure) {
+      axios
+        .delete(`/api/v1/foods/${currentFood.id}`)
+        .then((resp) => {
+          props.history.push("/foods");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
+  return (
+    <>
+      <h1>Editing Food</h1>
+      <div>
+        <div>
+          <label htmlFor="name">Current Content</label>
+          <InputName
+            type="text"
+            name="content"
+            value={currentFood.content}
+            onChange={handleInputChange}
+          />
+          <div>
+            <span>Current Status</span>
+            <br />
+            <CurrentStatus>
+              {currentFood.complete ? "Completed" : "Uncompleted"}
+            </CurrentStatus>
+          </div>
+        </div>
+        {currentFood.complete ? (
+          <IsCompletedButton onClick={() => updateIsCompleted(currentFood)}>
+            Uncompleted
+          </IsCompletedButton>
+        ) : (
+          <IsCompletedButton onClick={() => updateIsCompleted(currentFood)}>
+            Completed
+          </IsCompletedButton>
+        )}
+        <EditButton onClick={updateFood}>Update</EditButton>
+        <DeleteButton onClick={deleteFood}>Delete</DeleteButton>
+      </div>
+    </>
+  );
 }
 
 export default EditFood;
